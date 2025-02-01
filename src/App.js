084660e478e713634e15443d169c4400ddb13280
braypbsxzz <braylon.hecimovich@icloud.com>
2025-02-01 18:29:05 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <img src="./images.jpeg" className="App-logo" alt="logo" />
+        <p>
+          Will you be my valentiNina?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          I love you
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
